b4edbde8de38444a4a8255be1452937aaebea78e
McClone <abc15606@163.com>
2017-10-07 16:57:01 +0800

    metrics jmx

--- src/main/java/org/mcclone/Metrics.java
+++ src/main/java/org/mcclone/Metrics.java
@@ -1,6 +1,7 @@
 package org.mcclone;
 
 import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import org.apache.commons.lang3.RandomUtils;
@@ -15,12 +16,14 @@ public class Metrics {
     private static final MetricRegistry metrics = new MetricRegistry();
 
     private static ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics).build();
+    private static JmxReporter jmxReporter = JmxReporter.forRegistry(metrics).build();
 
     public static void main(String[] args) throws InterruptedException {
+        jmxReporter.start();
         reporter.start(3, TimeUnit.SECONDS);
         Timer timer = metrics.timer("timer");
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 500; i++) {
             Timer.Context context = timer.time();
             try {
                 Thread.sleep(RandomUtils.nextLong(3000, 4000));
